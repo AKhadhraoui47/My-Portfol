1075720e07bf1ab69145b8c0baa800794bbfd47e
AKhadhraoui47 <bkhadhraoui870@gmail.com>
2024-12-27 14:26:58 +0100

    Almost there

--- js/main.js
+++ js/main.js
@@ -41,5 +41,43 @@ AOS.init({
   once: true,
 });
 
+////// Projects //////
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollElements = document.querySelectorAll(".pro-scroll-anim");
+
+  const elementInView = (el, percentageScroll = 100) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+      elementTop <=
+      (window.innerHeight || document.documentElement.clientHeight) * (percentageScroll / 100)
+    );
+  };
+
+  const displayScrollElement = (element) => {
+    element.classList.add("visible");
+  };
+
+  const hideScrollElement = (element) => {
+    element.classList.remove("visible");
+  };
+
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el, 75)) {
+        displayScrollElement(el);
+      } else {
+        hideScrollElement(el);
+      }
+    });
+  };
+
+  window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+  });
+
+  handleScrollAnimation();
+});
+//////////////////////
 
 
